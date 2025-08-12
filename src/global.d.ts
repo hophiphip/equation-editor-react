e0309a3096b7669b28905d094874d52114f99823
hophiphip <nighop@yandex.ru>
2025-08-12 15:22:13 +0400

    chore: extended window with `jQuery` property

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,6 +2,10 @@ import { MathQuill } from "./types";
 
 declare global {
   var MathQuill: MathQuill;
+
+  interface Window {
+    jQuery: JQueryStatic;
+  }
 }
 
 export {};
--- src/jquery.ts
+++ src/jquery.ts
@@ -1,8 +1,6 @@
 // Import JQuery, required for the functioning of the equation editor
 import jQuery from "jquery";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
 window.jQuery = jQuery;
 
 export { jQuery };
